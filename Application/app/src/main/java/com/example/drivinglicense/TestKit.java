5afc731b05a9cd6d5c838573ca4077a6dd6e71a1
HuongP <56154599+pophuong2610@users.noreply.github.com>
2021-05-06 23:48:22 +0700

    update2
    hi guys, t làm xong layout phần Main and TestKit (thi theo bộ đề). phần Main có các Button t đã custom layour change background color trong file custom_btn1.xml rồi mà hong được. :(. ai fix giúp t nốt phần đấy nhé. thanks!!!
    
    Co-Authored-By: huyhoang181200390 <73602361+huyhoang181200390@users.noreply.github.com>
    Co-Authored-By: Nguyễn Hữu Thảo <49112998+NguyenThao1912@users.noreply.github.com>
    Co-Authored-By: binbun <43144399+binbun@users.noreply.github.com>

---
+++ Application/app/src/main/java/com/example/drivinglicense/TestKit.java
@@ -0,0 +1,16 @@
+package com.example.drivinglicense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class TestKit extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_kit);
+    }
+}
