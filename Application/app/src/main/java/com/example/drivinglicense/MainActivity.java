8761f9c1df213988c9026a4150f2b61138184270
Nguyễn Hữu Thảo <49112998+NguyenThao1912@users.noreply.github.com>
2021-05-07 20:26:05 +0700

    làm tiếp nha ae

--- Application/app/src/main/java/com/example/drivinglicense/MainActivity.java
+++ Application/app/src/main/java/com/example/drivinglicense/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.drivinglicense;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.drivinglicense.db.DBManager;
 
@@ -22,5 +27,41 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.option_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.change_licence:
+            {
+                Toast.makeText(this, "icon thay bằng lái click", Toast.LENGTH_SHORT).show();
+                //TODO Mở màn hình thay loại bằng lái
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void Dethi(View view)
+    {
+        Toast.makeText(this, "Thi sát hạch click", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,TestKit.class);
+        startActivity(intent);
+    }
+    public void DeNgauNhien(View view)
+    {
+        Toast.makeText(this, "Đề ngẫu nhiên click", Toast.LENGTH_SHORT).show();
+        // TODO Mở màn hình đề ngẫu nhiên
+    }
+    public void LoaiBienBao(View view)
+    {
+        Toast.makeText(this, "Thông tin biển báo click", Toast.LENGTH_SHORT).show();
+        // TODo mở màn hình loại biển báo
+    }
+    public void LoiKhuyen(View view)
+    {
+        Toast.makeText(this, "Lời khuyên click", Toast.LENGTH_SHORT).show();
+        // ToDo mở màn hình lời khuyên
     }
 }
--- Application/app/src/main/java/com/example/drivinglicense/db/DBHelper.java
+++
@@ -1,7 +0,0 @@
-package com.example.drivinglicense.db;
-
-import com.huma.room_for_asset.RoomAsset;
-
-public class DBHelper {
-
-}
