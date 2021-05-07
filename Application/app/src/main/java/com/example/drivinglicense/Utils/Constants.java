be5a4a7717d61e5940136782dbd50b4730c081bc
Nguyễn Hữu Thảo <49112998+NguyenThao1912@users.noreply.github.com>
2021-05-07 21:29:37 +0700

    biến mặc định

---
+++ Application/app/src/main/java/com/example/drivinglicense/Utils/Constants.java
@@ -0,0 +1,18 @@
+package com.example.drivinglicense.Utils;
+
+public class Constants {
+    public static String TABLE_QUESTION = "ZQUESTION";
+    public static String TABLE_LICENSE = "ZLICENSE";
+    public static String TABLE_QUESTION_TYPE = "ZQUESTIONTYPE";
+    public static String TABLE_SIGN = "ZSIGN";
+    public static String TABLE_SIGN_CATEGORY = "ZSIGNCATEGORY";
+    public static String TABLE_TEST = "ZTEST";
+    public static String TABLE_TEST_QUEST = "ZTESTQUEST";
+    public static String TABLE_TIP = "ZTIP";
+    public static String TABLE_TIP_CATEGORY = "ZTIPCATEGORY";
+    public static String TABLE_TIP_TYPE = "ZTIPTYPE";
+    public static String TABLE_NOTICE_BOARD = "TABLE_NOTICE_BOARD";
+    public static String TABLE_NOTICE_BOARDTYPE = "TABLE_NOTICE_BOARD_TYPE";
+    public static String TABLE_NUMBER_QUESTION_PER_TYPE = "ZNUMBERQUESTIONPERTYPE";
+
+}
